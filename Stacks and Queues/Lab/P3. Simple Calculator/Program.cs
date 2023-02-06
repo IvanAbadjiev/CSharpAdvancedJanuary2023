08dc604768c2f8c189270933e82535db87e5009d
IvanAbadjiev <ivan087611@gmail.com>
2023-02-06 22:03:55 +0200

    Stacks and Queues
    • Stack (first-in, last-out);
    • Stack Methods (Push, Pop, Peek, ToArray, Contains, Count);
    • Queue (first-in, first-out);
    • Queue Methods (Enqueue, Dequeue, Peek, ToArray, Contains, Count)

---
+++ Stacks and Queues/Lab/P3. Simple Calculator/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace P3._Simple_Calculator
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string[] input = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            // input.Reverse();
+            var stack = new Stack<string>(input.Reverse());
+
+            int num1 = int.Parse(stack.Pop());
+            int sum = num1;
+            while (stack.Count > 0)
+            {
+                if (stack.Pop() == "+")
+                {
+                    int num2 = int.Parse(stack.Pop());
+                    sum += num2;
+
+                }
+                else
+                {
+                    int num2 = int.Parse(stack.Pop());
+                    sum -= num2;
+
+
+                }
+
+
+
+
+
+            }
+            Console.WriteLine(sum);
+
+
+
+        }
+    }
+}
