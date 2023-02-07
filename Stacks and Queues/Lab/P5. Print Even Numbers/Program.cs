3cf1688534d3ff95905444080b02355c92bccbcd
IvanAbadjiev <ivan087611@gmail.com>
2023-02-07 20:28:05 +0200

    Stacks and Queues
    • Stack (first-in, last-out);
    • Stack Methods (Push, Pop, Peek, ToArray, Contains, Count);
    • Queue (first-in, first-out);
    • Queue Methods (Enqueue, Dequeue, Peek, ToArray, Contains, Count)

---
+++ Stacks and Queues/Lab/P5. Print Even Numbers/Program.cs
@@ -0,0 +1,30 @@
+﻿namespace P5._Print_Even_Numbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] input = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+
+
+            Queue<int> ints = new Queue<int>();
+
+            foreach (int num in input)
+            {
+                if (num % 2 == 0)
+                {
+                    ints.Enqueue(num);
+                }
+            }
+
+            Console.WriteLine(string.Join(", ", ints));
+        }
+    }
+}
