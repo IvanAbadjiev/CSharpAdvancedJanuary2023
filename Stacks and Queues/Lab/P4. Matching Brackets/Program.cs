7263918020466edc5187a2f06d178ff2604bd733
IvanAbadjiev <ivan087611@gmail.com>
2023-02-07 20:27:13 +0200

    Stacks and Queues
    • Stack (first-in, last-out);
    • Stack Methods (Push, Pop, Peek, ToArray, Contains, Count);
    • Queue (first-in, first-out);
    • Queue Methods (Enqueue, Dequeue, Peek, ToArray, Contains, Count)

---
+++ Stacks and Queues/Lab/P4. Matching Brackets/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace P4._Matching_Brackets
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input = Console.ReadLine();
+
+            var stack = new Stack<int>();
+
+            for (int i = 0; i < input.Length; i++)
+            {
+                char c = input[i];
+                if (c == '(')
+                {
+                    stack.Push(i);
+                }
+                else if (c == ')')
+                {
+                    int startIndex = stack.Pop();
+                    int endIndex = i;
+
+                    string subStr = input.Substring(startIndex, endIndex - startIndex + 1);
+                    Console.WriteLine(subStr);
+                }
+
+
+            }
+
+        }
+    }
+}
