5714c400a783af766475d2c87b291ee8a2d0cec4
IvanAbadjiev <ivan087611@gmail.com>
2023-02-07 20:28:23 +0200

    Stacks and Queues
    • Stack (first-in, last-out);
    • Stack Methods (Push, Pop, Peek, ToArray, Contains, Count);
    • Queue (first-in, first-out);
    • Queue Methods (Enqueue, Dequeue, Peek, ToArray, Contains, Count)

---
+++ Stacks and Queues/Lab/P6. Supermarket/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace P6._Supermarket
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string input;
+            var people = new Queue<string>();
+            while ((input = Console.ReadLine()) != "End")
+            {
+
+                if (input != "Paid")
+                {
+                    people.Enqueue(input);
+                }
+                else
+                {
+                    Console.WriteLine(string.Join(Environment.NewLine, people));
+                    people.Clear();
+                }
+
+
+            }
+            Console.WriteLine($"{people.Count} people remaining.");
+
+        }
+    }
+}
