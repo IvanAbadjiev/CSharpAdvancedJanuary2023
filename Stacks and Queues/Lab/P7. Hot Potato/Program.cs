3da91f747f93446e0ac700c5c2932c67d2aad758
IvanAbadjiev <ivan087611@gmail.com>
2023-02-08 19:59:37 +0200

    Stacks and Queues
    • Stack (first-in, last-out);
    • Stack Methods (Push, Pop, Peek, ToArray, Contains, Count);
    • Queue (first-in, first-out);
    • Queue Methods (Enqueue, Dequeue, Peek, ToArray, Contains, Count)

---
+++ Stacks and Queues/Lab/P7. Hot Potato/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace P7._Hot_Potato
+{
+    using System;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string[] childs = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+            int n = int.Parse(Console.ReadLine());
+            var queue = new Queue<string>(childs);
+
+            while (queue.Count > 1)
+            {
+                for (int i = 1; i <= n - 1; i++)
+                {
+                    var child = queue.Dequeue();
+                    queue.Enqueue(child);
+                }
+                var lostChild = queue.Dequeue();
+                Console.WriteLine($"Removed {lostChild}");
+            }
+            var lastChild = queue.Dequeue();
+            Console.WriteLine($"Last is {lastChild}");
+
+
+
+
+
+        }
+    }
+}
