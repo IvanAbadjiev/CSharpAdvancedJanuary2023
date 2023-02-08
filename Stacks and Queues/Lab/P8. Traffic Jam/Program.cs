21513d3f3ba1b4b52ebcb8e8aa527c8286655a62
IvanAbadjiev <ivan087611@gmail.com>
2023-02-08 20:00:08 +0200

    Stacks and Queues
    • Stack (first-in, last-out);
    • Stack Methods (Push, Pop, Peek, ToArray, Contains, Count);
    • Queue (first-in, first-out);
    • Queue Methods (Enqueue, Dequeue, Peek, ToArray, Contains, Count)

---
+++ Stacks and Queues/Lab/P8. Traffic Jam/Program.cs
@@ -0,0 +1,60 @@
+﻿namespace P8._Traffic_Jam
+{
+    using System;
+    using System.Collections;
+    using System.Collections.Generic;
+
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine());
+            var cars = new Queue<string>();
+            int count = 0;
+            string car;
+            while ((car = Console.ReadLine()) != "end")
+            {
+                if (car == "green")
+                {
+                    if (cars.Count >= n)
+                    {
+                        for (int i = 0; i < n; i++)
+                        {
+                            var currCar = cars.Dequeue();
+                            Console.WriteLine($"{currCar} passed!");
+                            count++;
+                        }
+
+                    }
+                    else if (cars.Count > 0)
+                    {
+                        for (int i = 0; i <= cars.Count; i++)
+                        {
+                            var currCar = cars.Dequeue();
+                            Console.WriteLine($"{currCar} passed!");
+                            count++;
+                        }
+
+                    }
+
+
+                }
+                else
+                {
+                    cars.Enqueue(car);
+
+                }
+
+            }
+            Console.WriteLine($"{count} cars passed the crossroads.");
+
+
+
+
+
+
+
+
+        }
+    }
+}
