b3bd4a399cb69840c83eaa636eb1b3710934bb42
IvanAbadjiev <ivan087611@gmail.com>
2023-02-10 21:15:23 +0200

    • Multidimensional Arrays (creating, accessing elements, reading and printing); • Jagged Arrays (Arrays of Arrays).

---
+++ Multidimensional Arrays/Lab/P3. Primary Diagonal/PrimaryDiagonal.cs
@@ -0,0 +1,28 @@
+﻿//read matrix
+int rowsCount = int.Parse(Console.ReadLine());
+int colsCount = rowsCount;
+int[,] matrix = new int[rowsCount, colsCount];
+for (int row = 0; row < rowsCount; row++)
+{
+    int[] curr = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+    for (int col = 0; col < colsCount; col++)
+    {
+        matrix[row, col] = curr[col];
+    }
+}
+
+//Sum matrix diagonale
+
+int[] result = new int[rowsCount];
+
+for (int row = 0; row < rowsCount; row++)
+    for (int col = 0; col <= row; col++)
+    {
+        result[row] = matrix[row, col];
+    }
+
+
+Console.WriteLine(result.Sum());
