835250a755199d6985e4ba3d8d8d54d2f90953b2
IvanAbadjiev <ivan087611@gmail.com>
2023-02-10 21:14:21 +0200

    • Multidimensional Arrays (creating, accessing elements, reading and printing); • Jagged Arrays (Arrays of Arrays).

---
+++ Multidimensional Arrays/Lab/P1. Sum Matrix Elements/Program.cs
@@ -0,0 +1,43 @@
+﻿//reed matrix
+int[] input = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+(int rows, int cols) = (input[0], input[1]);
+int[,] matrix = new int[rows, cols];
+for (int row = 0; row < rows; row++)
+{
+    int[] curr = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+    for (int col = 0; col < cols; col++)
+    {
+        matrix[row, col] = curr[col];
+    }
+
+}
+
+//Print matrix
+//for (int row = 0; row < rows; row++)
+//{
+//    for (int col = 0; col < cols; col++)
+//    {
+//        Console.Write(matrix[row, col] + " ");
+//    }
+//    Console.WriteLine();
+//}
+
+
+long sum = 0;
+for (int row = 0; row < rows; row++)
+    for (int col = 0; col < cols; col++)
+    {
+        sum += matrix[row, col];
+    }
+
+Console.WriteLine(rows);
+Console.WriteLine(cols);
+Console.WriteLine(sum);
+
+
