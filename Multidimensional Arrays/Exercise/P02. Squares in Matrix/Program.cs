5fc014d88feaad8999ee2f91c7ed28cde29cbed6
IvanAbadjiev <ivan087611@gmail.com>
2023-02-12 10:55:58 +0200

    • Multidimensional Arrays (creating, accessing elements, reading and printing); • Jagged Arrays (Arrays of Arrays).

---
+++ Multidimensional Arrays/Exercise/P02. Squares in Matrix/Program.cs
@@ -0,0 +1,45 @@
+﻿int[] input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int rowCount = input[0];
+int colCount = input[1];
+
+char[,] matrix = new char[rowCount, colCount];
+BildMatrix(matrix);
+
+int squaresInMatrixCounter = 0;
+for (int row = 0; row < rowCount - 1; row++)
+{
+    for (int col = 0; col < colCount - 1; col++)
+    {
+        if (matrix[row, col] == matrix[row, col + 1]
+            && matrix[row, col] == matrix[row + 1, col]
+            && matrix[row, col] == matrix[row + 1, col + 1])
+        {
+            squaresInMatrixCounter++;
+        }
+    }
+}
+
+Console.WriteLine(squaresInMatrixCounter);
+
+
+
+void BildMatrix(char[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        char[] rowData = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(char.Parse)
+            .ToArray();
+        for (int col = 0; col < matrix.GetLength(1); col++)
+            matrix[row, col] = rowData[col];
+
+    }
+
+
+
+}
