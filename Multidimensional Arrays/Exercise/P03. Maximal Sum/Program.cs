e84055c68f941ccca4447a8405d110139bd90502
IvanAbadjiev <ivan087611@gmail.com>
2023-02-13 21:23:00 +0200

    • Multidimensional Arrays (creating, accessing elements, reading and printing); • Jagged Arrays (Arrays of Arrays).

---
+++ Multidimensional Arrays/Exercise/P03. Maximal Sum/Program.cs
@@ -0,0 +1,53 @@
+﻿int[] input = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int rowCount = input[0];
+int colCount = input[1];
+
+int[,] matrix = new int[rowCount, colCount];
+BildMatrix(matrix);
+
+int maxSum = int.MinValue;
+int rowForPrint = 0;
+int colForPrint = 0;
+
+for (int row = 0; row < rowCount - 2; row++)
+{
+    for (int col = 0; col < colCount - 2; col++)
+    {
+        int currentSum = matrix[row, col] + matrix[row, col + 1] + matrix[row, col + 2] +
+                  matrix[row + 1, col] + matrix[row + 1, col + 1] + matrix[row + 1, col + 2] +
+                  matrix[row + 2, col] + matrix[row + 2, col + 1] + matrix[row + 2, col + 2];
+        if (currentSum > maxSum)
+        {
+            maxSum = currentSum;
+            rowForPrint = row;
+            colForPrint = col;
+        }
+    }
+}
+Console.WriteLine("Sum " + "= " + maxSum);
+Console.WriteLine($"{matrix[rowForPrint, colForPrint]} {matrix[rowForPrint, colForPrint + 1]} {matrix[rowForPrint, colForPrint + 2]}{Environment.NewLine}" +
+    $"{matrix[rowForPrint + 1, colForPrint]} {matrix[rowForPrint + 1, colForPrint + 1]} {matrix[rowForPrint + 1, colForPrint + 2]}{Environment.NewLine}" +
+    $"{matrix[rowForPrint + 2, colForPrint]} {matrix[rowForPrint + 2, colForPrint + 1]} {matrix[rowForPrint + 2, colForPrint + 2]}");
+
+
+
+void BildMatrix(int[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        int[] rowData = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+        for (int col = 0; col < matrix.GetLength(1); col++)
+            matrix[row, col] = rowData[col];
+
+    }
+
+
+
+}
