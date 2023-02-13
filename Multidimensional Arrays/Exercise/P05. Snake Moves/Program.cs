4af379ff6e67cb205756167b2dee4e068535f458
IvanAbadjiev <ivan087611@gmail.com>
2023-02-13 21:24:45 +0200

    • Multidimensional Arrays (creating, accessing elements, reading and printing); • Jagged Arrays (Arrays of Arrays).

---
+++ Multidimensional Arrays/Exercise/P05. Snake Moves/Program.cs
@@ -0,0 +1,51 @@
+﻿int[] n = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int rowCount = 0; int colCount = 0;
+string inputText = Console.ReadLine();
+char[,] matrix = new char[rowCount, colCount];
+
+int index = 0;
+for (int row = 0; row < rowCount; row++)
+{
+    if (row % 2 == 0)
+    {
+        for (int col = 0; col < colCount; col++)
+        {
+            matrix[row, col] = inputText[index];
+            index++;
+            if (index >= inputText.Length)
+            {
+                index = 0;
+            }
+        }
+    }
+    else
+    {
+        for (int col = colCount - 1; col >= 0; col--)
+        {
+            matrix[row, col] = inputText[index];
+            index++;
+            if (index >= inputText.Length)
+            {
+                index = 0;
+            }
+        }
+    }
+}
+
+PrintMatrix(matrix);
+
+void PrintMatrix(char[,] matrix)
+{
+    for (int row = 0; row < matrix.GetLength(0); row++)
+    {
+        for (int col = 0; col < matrix.GetLength(1); col++)
+        {
+            Console.WriteLine(matrix[row, col]);
+        }
+        Console.WriteLine();
+    }
+}
