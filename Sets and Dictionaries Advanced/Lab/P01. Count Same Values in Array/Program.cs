2d910889955d697b1471ddce262208bc8af55bf2
IvanAbadjiev <ivan087611@gmail.com>
2023-02-14 22:01:50 +0200

    • Dictionary; • Multi-Dictionaries; • Set (HashSet, SortedSet).

---
+++ Sets and Dictionaries Advanced/Lab/P01. Count Same Values in Array/Program.cs
@@ -0,0 +1,17 @@
+﻿string[] inputText = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+Dictionary<string, int> repeatValue = new Dictionary<string, int>();
+
+for (int index = 0; index < inputText.Length; index++)
+{
+    if (!repeatValue.ContainsKey(inputText[index]))
+        repeatValue.Add(inputText[index], 0);
+
+    repeatValue[inputText[index]] += 1;
+
+}
+foreach (KeyValuePair<string, int> entry in repeatValue)
+{
+    Console.WriteLine($"{entry.Key} - {entry.Value} times");
+}
