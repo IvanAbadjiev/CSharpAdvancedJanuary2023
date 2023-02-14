96562af8e50fc283de345aa909b5a5c0e63043be
IvanAbadjiev <ivan087611@gmail.com>
2023-02-14 22:02:46 +0200

    • Dictionary; • Multi-Dictionaries; • Set (HashSet, SortedSet).

---
+++ Sets and Dictionaries Advanced/Lab/P03. Largest 3 Numbers/Program.cs
@@ -0,0 +1,12 @@
+﻿int[] inputNumber = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .OrderByDescending(n => n)
+    .Take(3)
+    .ToArray();
+
+
+Console.WriteLine(string.Join(" ", inputNumber));
+
+
+
