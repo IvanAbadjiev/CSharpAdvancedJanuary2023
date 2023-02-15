87dca6bdcd696a2cf2acf44aa4f4ab8f38c3c83e
IvanAbadjiev <ivan087611@gmail.com>
2023-02-15 20:30:41 +0200

    • Dictionary; • Multi-Dictionaries; • Set (HashSet, SortedSet).

---
+++ Sets and Dictionaries Advanced/Lab/P05. Cities by Continent and Country/Program.cs
@@ -0,0 +1,35 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+var continentInfo = new Dictionary<string, Dictionary<string, List<string>>>();
+
+for (int i = 0; i < n; i++)
+{
+    string[] curentContinentInfo = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+
+    string continent = curentContinentInfo[0];
+    string country = curentContinentInfo[1];
+    string city = curentContinentInfo[2];
+
+    if (!continentInfo.ContainsKey(continent))
+        continentInfo.Add(continent, new Dictionary<string, List<string>>());
+
+
+    if (!continentInfo[continent].ContainsKey(country))
+        continentInfo[continent].Add(country, new List<string>());
+
+    continentInfo[continent][country].Add(city);
+
+
+
+}
+
+foreach (var pair in continentInfo)
+{
+    Console.WriteLine($"{pair.Key}:");
+    foreach (var innerPair in pair.Value)
+    {
+        Console.WriteLine(" {0} -> {1}", innerPair.Key, string.Join(", ", innerPair.Value));
+    }
+}
