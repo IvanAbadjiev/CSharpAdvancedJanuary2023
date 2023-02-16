e9b2058596a7fca20804a83e8c03ade96be49caa
IvanAbadjiev <ivan087611@gmail.com>
2023-02-16 21:39:46 +0200

    • Dictionary; • Multi-Dictionaries; • Set (HashSet, SortedSet).

---
+++ Sets and Dictionaries Advanced/Lab/P07. Parking Lot/Program.cs
@@ -0,0 +1,32 @@
+﻿string command = string.Empty;
+
+var listCarNumber = new HashSet<string>();
+while ((command = Console.ReadLine()) != "END")
+{
+    string[] cmdArgs = command.Split(", ", StringSplitOptions.RemoveEmptyEntries);
+    string cmdType = cmdArgs[0];
+    string carNumber = cmdArgs[1];
+
+    if (cmdType == "IN")
+    {
+        listCarNumber.Add(carNumber);
+    }
+    if (cmdType == "OUT")
+    {
+        if (listCarNumber.Contains(carNumber))
+        {
+            listCarNumber.Remove(carNumber);
+        }
+    }
+}
+if (listCarNumber.Count >= 0)
+{
+    foreach (var carNumber in listCarNumber)
+    {
+        Console.WriteLine(carNumber);
+    }
+}
+else
+{
+    Console.WriteLine("Parking Lot is Empty");
+}
