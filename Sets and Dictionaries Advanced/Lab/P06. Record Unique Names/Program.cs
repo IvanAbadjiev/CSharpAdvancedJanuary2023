1492183beb7e19ce6fc4db174188b725d4129e15
IvanAbadjiev <ivan087611@gmail.com>
2023-02-16 21:39:22 +0200

    • Dictionary; • Multi-Dictionaries; • Set (HashSet, SortedSet).

---
+++ Sets and Dictionaries Advanced/Lab/P06. Record Unique Names/Program.cs
@@ -0,0 +1,13 @@
+﻿int n = int.Parse(Console.ReadLine());
+HashSet<string> listOfNames = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string currName = Console.ReadLine();
+    listOfNames.Add(currName);
+}
+
+foreach (var name in listOfNames)
+{
+    Console.WriteLine(name);
+}
