53f57417502aba81ccbed08fd4c5a9f8e63f5eb6
IvanAbadjiev <ivan087611@gmail.com>
2023-02-17 19:32:08 +0200

    • Dictionary; • Multi-Dictionaries; • Set (HashSet, SortedSet).

---
+++ Sets and Dictionaries Advanced/Exercises/P03. Periodic Table/Program.cs
@@ -0,0 +1,17 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+var element = new SortedSet<string>();
+for (int i = 0; i < n; i++)
+{
+    string[] line = Console.ReadLine()
+        .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        .ToArray();
+
+    for (int m = 0; m < line.Length; m++)
+    {
+        element.Add(line[m]);
+    }
+
+}
+
+Console.WriteLine(string.Join(" ", element));
