90442782d3818c9041cb65853b2b369740244dda
IvanAbadjiev <ivan087611@gmail.com>
2023-02-17 19:31:35 +0200

    • Dictionary; • Multi-Dictionaries; • Set (HashSet, SortedSet).

---
+++ Sets and Dictionaries Advanced/Exercises/P02. Sets of Elements/Program.cs
@@ -0,0 +1,25 @@
+﻿int[] inputInfo = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int n = inputInfo[0];
+int m = inputInfo[1];
+
+var numberN = new HashSet<int>();
+var numberM = new HashSet<int>();
+for (int i = 0; i < n; i++)
+{
+    int currNumber = int.Parse(Console.ReadLine());
+    numberN.Add(currNumber);
+}
+
+for (int i = 0; i < m; i++)
+{
+    int currNumber = int.Parse(Console.ReadLine());
+    numberM.Add(currNumber);
+}
+
+numberN.IntersectWith(numberM);
+
+Console.WriteLine(string.Join(" ", numberN));
