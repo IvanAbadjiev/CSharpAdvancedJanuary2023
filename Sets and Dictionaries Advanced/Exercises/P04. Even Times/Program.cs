feb8480107344829d735e6877b82017f19397162
IvanAbadjiev <ivan087611@gmail.com>
2023-02-17 19:32:36 +0200

    • Dictionary; • Multi-Dictionaries; • Set (HashSet, SortedSet).

---
+++ Sets and Dictionaries Advanced/Exercises/P04. Even Times/Program.cs
@@ -0,0 +1,21 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+var numberInfo = new Dictionary<int, int>();
+for (int i = 0; i < n; i++)
+{
+    int currNumber = int.Parse(Console.ReadLine());
+
+    if (!numberInfo.ContainsKey(currNumber))
+        numberInfo.Add(currNumber, 0);
+
+    numberInfo[currNumber]++;
+}
+
+foreach (var number in numberInfo)
+{
+    if (number.Value % 2 == 0)
+    {
+        Console.WriteLine(number.Key);
+    }
+
+}
