8a60f483c63d1297576f2eebef94d8c3ec2d0e4b
IvanAbadjiev <ivan087611@gmail.com>
2023-02-17 19:31:02 +0200

    • Dictionary; • Multi-Dictionaries; • Set (HashSet, SortedSet).

---
+++ Sets and Dictionaries Advanced/Exercises/P01. Unique Usernames/Program.cs
@@ -0,0 +1,11 @@
+﻿int n = int.Parse(Console.ReadLine());
+
+var nameList = new HashSet<string>();
+
+for (int i = 0; i < n; i++)
+{
+    string currName = Console.ReadLine();
+    nameList.Add(currName);
+}
+
+Console.WriteLine(string.Join(Environment.NewLine, nameList));
