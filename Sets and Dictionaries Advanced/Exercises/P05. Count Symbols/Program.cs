fc4dca39564a57b2a3609ce983f4322db287cffd
IvanAbadjiev <ivan087611@gmail.com>
2023-02-18 21:46:06 +0200

    • Dictionary; • Multi-Dictionaries; • Set (HashSet, SortedSet).

---
+++ Sets and Dictionaries Advanced/Exercises/P05. Count Symbols/Program.cs
@@ -0,0 +1,16 @@
+﻿string text = Console.ReadLine();
+
+var textInfo = new SortedDictionary<char, int>();
+for (int i = 0; i < text.Length; i++)
+{
+    char curSimbol = text[i];
+    if (!textInfo.ContainsKey(curSimbol))
+        textInfo.Add(curSimbol, 0);
+
+    textInfo[curSimbol]++;
+}
+
+foreach (var symbol in textInfo)
+{
+    Console.WriteLine($"{symbol.Key}: {symbol.Value} time/s");
+}
