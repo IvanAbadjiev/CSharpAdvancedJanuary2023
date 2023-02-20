5e7edcbb569c5327e6b4fa09ef9b051c7bb70378
IvanAbadjiev <ivan087611@gmail.com>
2023-02-20 21:53:45 +0200

    Streams, Files and Directories
    • What are Streams?;
    • Readers and Writers;
    • File Streams;
    • File Class;
    • Dictionary Class.

---
+++ Streams Files and Directories/Lab/FolderSize/FolderSize.cs
@@ -0,0 +1,48 @@
+﻿namespace FolderSize
+{
+    using System;
+    using System.IO;
+    public class FolderSize
+    {
+        static void Main(string[] args)
+        {
+            string folderPath = @"..\..\..\Files\TestFolder";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            GetFolderSize(folderPath, outputPath);
+        }
+
+        public static void GetFolderSize(string folderPath, string outputFilePath)
+        {
+
+            StreamWriter streamWriter = new StreamWriter(outputFilePath);
+            using (streamWriter)
+            {
+                streamWriter.WriteLine(GetFolderSize(folderPath) / 1024 + " KB");
+
+            }
+
+        }
+
+        public static decimal GetFolderSize(string path)
+        {
+            string[] filePaths = Directory.GetFiles(path);
+
+            decimal size = 0;
+
+            foreach (var filePath in filePaths)
+            {
+                FileInfo info = new FileInfo(filePath);
+                size += info.Length;
+
+            }
+
+            foreach (var dirPaths in Directory.GetDirectories(path))
+            {
+                size += GetFolderSize(dirPaths);
+            }
+            return size;
+
+        }
+    }
+}
