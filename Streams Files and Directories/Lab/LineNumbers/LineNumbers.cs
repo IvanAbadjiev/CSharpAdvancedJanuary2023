0c60f3bc06eec1784bf548a4fe16ee531bbeccf3
IvanAbadjiev <ivan087611@gmail.com>
2023-02-20 21:54:15 +0200

    Streams, Files and Directories
    • What are Streams?;
    • Readers and Writers;
    • File Streams;
    • File Class;
    • Dictionary Class.

---
+++ Streams Files and Directories/Lab/LineNumbers/LineNumbers.cs
@@ -0,0 +1,38 @@
+﻿namespace LineNumbers
+{
+    using System.IO;
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputPath = @"..\..\..\Files\input.txt";
+            string outputPath = @"..\..\..\Files\output.txt";
+
+            RewriteFileWithLineNumbers(inputPath, outputPath);
+        }
+
+        public static void RewriteFileWithLineNumbers(string inputFilePath, string outputFilePath)
+        {
+            StreamReader streamReader = new StreamReader(inputFilePath);
+
+            using (streamReader)
+            {
+                StreamWriter streamWriter = new StreamWriter(outputFilePath);
+                using (streamWriter)
+                {
+
+                    string line = streamReader.ReadLine();
+                    int row = 0;
+                    while (line != null)
+                    {
+                        streamWriter.WriteLine($"{++row}. {line}");
+                        line = streamReader.ReadLine();
+                    }
+                }
+            }
+
+
+
+        }
+    }
+}
