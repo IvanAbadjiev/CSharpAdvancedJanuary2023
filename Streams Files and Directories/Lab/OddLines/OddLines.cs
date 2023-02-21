5e556570c30c35c56057b461aea7cea149dac8fa
IvanAbadjiev <ivan087611@gmail.com>
2023-02-21 22:19:18 +0200

    Streams, Files and Directories
    • What are Streams?;
    • Readers and Writers;
    • File Streams;
    • File Class;
    • Dictionary Class.

---
+++ Streams Files and Directories/Lab/OddLines/OddLines.cs
@@ -0,0 +1,52 @@
+﻿namespace OddLines
+{
+    using System.IO;
+
+    public class OddLines
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\Files\input.txt";
+            string outputFilePath = @"..\..\..\Files\output.txt";
+
+            ExtractOddLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ExtractOddLines(string inputFilePath, string outputFilePath)
+        {
+            StreamReader streamReader = new StreamReader(inputFilePath);
+
+            using (streamReader)
+            {
+
+                StreamWriter streamWriter = new StreamWriter(outputFilePath);
+
+                using (streamWriter)
+                {
+                    int row = 0;
+                    while (true)
+                    {
+                        string input = streamReader.ReadLine();
+                        if (input == null)
+                            break;
+
+                        if (row % 2 != 0)
+                        {
+                            streamWriter.WriteLine(input);
+
+                        }
+                        row++;
+
+
+
+                    }
+                }
+            }
+
+
+
+
+
+        }
+    }
+}
