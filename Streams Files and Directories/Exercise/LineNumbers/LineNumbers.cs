111d26eb844c081648db5780271e07868b3d8a34
IvanAbadjiev <ivan087611@gmail.com>
2023-02-23 21:53:33 +0200

    Streams, Files and Directories
    • What are Streams?;
    • Readers and Writers;
    • File Streams;
    • File Class;
    • Dictionary Class.

---
+++ Streams Files and Directories/Exercise/LineNumbers/LineNumbers.cs
@@ -0,0 +1,35 @@
+﻿namespace LineNumbers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.IO;
+    using System.Linq;
+
+    public class LineNumbers
+    {
+        static void Main()
+        {
+            string inputFilePath = @"..\..\..\text.txt";
+            string outputFilePath = @"..\..\..\output.txt";
+
+            ProcessLines(inputFilePath, outputFilePath);
+        }
+
+        public static void ProcessLines(string inputFilePath, string outputFilePath)
+        {
+            string[] lines = File.ReadAllLines(inputFilePath);
+            int count = 0;
+            List<string> outLines = new List<string>();
+            foreach (var line in lines)
+            {
+                int letherCount = line.Count(char.IsLetter);
+                int symbolCount = line.Count(char.IsPunctuation);
+                count++;
+                string moodLine = $"Line {count}: {line} ({letherCount})({symbolCount})";
+                outLines.Add(moodLine);
+            }
+
+            File.WriteAllLines(outputFilePath, outLines);
+        }
+    }
+}
