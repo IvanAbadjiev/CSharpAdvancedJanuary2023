a02fadef9c6f830c32ad8874a8270c39480353d5
IvanAbadjiev <ivan087611@gmail.com>
2023-02-24 21:01:48 +0200

    Functional Programming
    • Functional Programming;
    • Lambda Expressions;
    • Action, Func (passing functions to methods).

---
+++ Functional Programming/Lab/P02. Sum Numbers/SumNumbers.cs
@@ -0,0 +1,9 @@
+﻿var nums = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+int sum = nums.Sum();
+
+Console.WriteLine(nums.Length);
+Console.WriteLine(sum);
