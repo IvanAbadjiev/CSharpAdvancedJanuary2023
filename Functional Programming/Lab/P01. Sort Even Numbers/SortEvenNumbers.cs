3da98600337a7a3422f6d66a130e0ba15bc47dd8
IvanAbadjiev <ivan087611@gmail.com>
2023-02-24 21:00:24 +0200

    Functional Programming
    • Functional Programming;
    • Lambda Expressions;
    • Action, Func (passing functions to methods).

---
+++ Functional Programming/Lab/P01. Sort Even Numbers/SortEvenNumbers.cs
@@ -0,0 +1,9 @@
+﻿var evenNumsOnliOrdared = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .Where(x => x % 2 == 0)
+    .OrderBy(x => x)
+    .ToArray();
+
+Console.WriteLine(string.Join(", ", evenNumsOnliOrdared));
+
