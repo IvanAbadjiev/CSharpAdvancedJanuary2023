9bb027675eb869d47da2e9a6348dcac994fdb850
IvanAbadjiev <ivan087611@gmail.com>
2023-03-13 21:49:01 +0200

    Functional Programming
    • Functional Programming;
    • Lambda Expressions;
    • Action, Func (passing functions to methods).

---
+++ Functional Programming/Lab/P04. Add VAT/AddVAT.cs
@@ -0,0 +1,12 @@
+﻿Func<double, double> addVAT = n => (n + (n / 100) * 20);
+
+var nums = Console.ReadLine()
+    .Split(", ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(double.Parse)
+    .Select(addVAT)
+    .ToArray();
+
+foreach (var num in nums)
+{
+    Console.WriteLine($"{num:f2}");
+}
