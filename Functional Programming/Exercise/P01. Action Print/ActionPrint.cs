24d501e909dbf38d0e30b5f128de54376b13ed63
IvanAbadjiev <ivan087611@gmail.com>
2023-03-20 21:27:50 +0200

    Functional Programming
    • Functional Programming;
    • Lambda Expressions;
    • Action, Func (passing functions to methods).

---
+++ Functional Programming/Exercise/P01. Action Print/ActionPrint.cs
@@ -0,0 +1,7 @@
+﻿Action<string[]> printNames = n => Console.WriteLine(string.Join(Environment.NewLine, n));
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+printNames(names);
