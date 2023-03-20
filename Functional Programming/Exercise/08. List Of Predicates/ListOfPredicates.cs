897d4b8748b305d62e8706f38aadc10a69ed3907
IvanAbadjiev <ivan087611@gmail.com>
2023-03-20 21:27:23 +0200

    Functional Programming
    • Functional Programming;
    • Lambda Expressions;
    • Action, Func (passing functions to methods).

---
+++ Functional Programming/Exercise/08. List Of Predicates/ListOfPredicates.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections.Generic;
+using System.Security.Principal;
+
+Func<int, List<int>, List<int>> filterNumber = (endRange, numbers) =>
+{
+    List<int> result = new();
+    for (int i = 1; i <= endRange; i++)
+    {
+        bool isValid = true;
+        foreach (var number in numbers)
+        {
+            if (i % number == 0)
+            {
+
+            }
+            else
+            {
+                isValid = false;
+            }
+        }
+        if (isValid)
+        {
+            result.Add(i);
+        }
+    }
+
+    return result;
+};
+
+int endRange = int.Parse(Console.ReadLine());
+List<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+List<int> printNumbers = filterNumber(endRange, numbers);
+
+Console.WriteLine(string.Join(" ", printNumbers));
+
