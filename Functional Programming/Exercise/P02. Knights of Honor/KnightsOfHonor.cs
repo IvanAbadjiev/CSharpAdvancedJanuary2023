9f364bff3eb6c66a9c3c83f50a782bb6c75f7ba8
IvanAbadjiev <ivan087611@gmail.com>
2023-03-21 21:37:38 +0200

    Functional Programming
    • Functional Programming;
    • Lambda Expressions;
    • Action, Func (passing functions to methods).

---
+++ Functional Programming/Exercise/P02. Knights of Honor/KnightsOfHonor.cs
@@ -0,0 +1,14 @@
+﻿Action<string[], string> printNames = (n, t) =>
+{
+    foreach (var name in n)
+    {
+        Console.WriteLine($"{t} {name}");
+    }
+};
+
+string[] names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToArray();
+
+const string title = "Sir";
+printNames(names, title);
