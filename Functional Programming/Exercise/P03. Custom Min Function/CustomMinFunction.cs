dbbf66f8c285aebc273942e4ceb1497969ab122f
IvanAbadjiev <ivan087611@gmail.com>
2023-03-23 20:30:20 +0200

    Functional Programming
    • Functional Programming;
    • Lambda Expressions;
    • Action, Func (passing functions to methods).

---
+++ Functional Programming/Exercise/P03. Custom Min Function/CustomMinFunction.cs
@@ -0,0 +1,22 @@
+﻿Func<HashSet<int>, int> minNumberInSet = n =>
+{
+    int minNumber = int.MaxValue;
+
+    foreach (var number in n)
+    {
+        if (number < minNumber)
+        {
+            minNumber = number;
+        }
+    }
+    return minNumber;
+
+};
+
+
+HashSet<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToHashSet();
+
+Console.WriteLine(minNumberInSet(numbers));
