b3254835cc36a36c639f414451d434f1e1b27589
IvanAbadjiev <ivan087611@gmail.com>
2023-03-23 20:31:12 +0200

    Functional Programming
    • Functional Programming;
    • Lambda Expressions;
    • Action, Func (passing functions to methods).

---
+++ Functional Programming/Exercise/P04. Find Evens or Odds/FindEvensOrOdds.cs
@@ -0,0 +1,44 @@
+﻿Func<int, int, List<int>> genereteRange = (start, end) =>
+{
+    List<int> range = new();
+    for (int i = start; i <= end; i++)
+    {
+        range.Add(i);
+    }
+
+    return range;
+
+};
+
+Predicate<int> match;
+int[] range = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToArray();
+
+List<int> numbers = genereteRange(range[0], range[1]);
+string command = Console.ReadLine();
+if (command == "even")
+{
+    match = number => number % 2 == 0;
+
+}
+else if (command == "odd")
+{
+    match = number => number % 2 != 0;
+
+}
+else
+{
+    match = number => false;
+}
+
+foreach (int number in numbers)
+{
+    if (match(number))
+    {
+        Console.Write($"{number} ");
+
+
+    }
+}
