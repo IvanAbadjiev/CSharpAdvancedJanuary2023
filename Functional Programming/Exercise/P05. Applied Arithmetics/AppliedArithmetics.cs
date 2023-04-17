b37e540ca14010aded782a3dcb8d3658d15d2fb1
IvanAbadjiev <ivan087611@gmail.com>
2023-04-17 20:59:04 +0300

    Functional Programming
    • Functional Programming;
    • Lambda Expressions;
    • Action, Func (passing functions to methods).

---
+++ Functional Programming/Exercise/P05. Applied Arithmetics/AppliedArithmetics.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using System.Security.Principal;
+
+Func<string, List<int>, List<int>> operationInNumbers = (command, numbers) =>
+{
+    List<int> result = new();
+
+    foreach (var number in numbers)
+    {
+
+        switch (command)
+        {
+            case "add":
+                result.Add(number + 1);
+
+                break;
+            case "subtract":
+                result.Add(number - 1);
+                break;
+            case "multiply":
+                result.Add(number * 2);
+                break;
+            default:
+                break;
+        }
+    }
+
+    return result;
+};
+
+List<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+string command;
+while ((command = Console.ReadLine()) != "end")
+{
+    if (command == "print")
+    {
+        Console.WriteLine(string.Join(" ", numbers));
+    }
+    else
+    {
+        numbers = operationInNumbers(command, numbers);
+    }
+
+
+
+}
+
