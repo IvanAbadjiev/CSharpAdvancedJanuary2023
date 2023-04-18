a86ea7b51955592ad889512c3d458ca494518cc5
IvanAbadjiev <ivan087611@gmail.com>
2023-04-18 21:36:47 +0300

    Functional Programming
    • Functional Programming;
    • Lambda Expressions;
    • Action, Func (passing functions to methods).

---
+++ Functional Programming/Exercise/P06. Reverse And Exclude/ReverseAndExclude.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections.Generic;
+using System.Security.Principal;
+
+Func<int, List<int>, List<int>> filterNumber = (divider, numbers) =>
+{
+    List<int> result = new();
+
+    foreach (var number in numbers)
+    {
+        if (number % divider != 0)
+            result.Add(number);
+
+    }
+
+    return result;
+};
+
+List<int> numbers = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .Select(int.Parse)
+    .ToList();
+
+int divider = int.Parse(Console.ReadLine());
+
+numbers = filterNumber(divider, numbers);
+
+numbers.Reverse();
+
+Console.WriteLine(string.Join(" ", numbers));
