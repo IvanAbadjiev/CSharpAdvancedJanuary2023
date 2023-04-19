d4f4ada8da38a03ddd05f823c758801d00acb166
IvanAbadjiev <ivan087611@gmail.com>
2023-04-19 19:29:46 +0300

    Functional Programming
    • Functional Programming;
    • Lambda Expressions;
    • Action, Func (passing functions to methods).

---
+++ Functional Programming/Exercise/P07. Predicate For Names/PredicateForNames.cs
@@ -0,0 +1,27 @@
+﻿Func<int, List<string>, List<string>> filterNames = (length, names) =>
+{
+    List<string> result = new();
+
+    foreach (var name in names)
+    {
+        if (name.Length <= length)
+        {
+            result.Add(name);
+        }
+
+    }
+    return result;
+
+};
+
+
+int length = int.Parse(Console.ReadLine());
+
+List<string> names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+
+names = filterNames(length, names);
+
+Console.WriteLine(string.Join(Environment.NewLine, names));
