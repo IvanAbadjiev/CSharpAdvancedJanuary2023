6d328297b73f67e0a462be8921a7a3d0f5f227db
IvanAbadjiev <ivan087611@gmail.com>
2023-04-21 21:18:35 +0300

    Functional Programming
    • Functional Programming;
    • Lambda Expressions;
    • Action, Func (passing functions to methods).

---
+++ Functional Programming/Exercise/P11. TriFunction/TriFunction.cs
@@ -0,0 +1,21 @@
+﻿using System.Diagnostics.CodeAnalysis;
+using System.Diagnostics.SymbolStore;
+
+Func<string, int, bool> checkLength = (name, sum)
+    => name.Sum(ch => ch) >= sum;
+
+
+Func<List<string>, int, Func<string, int, bool>, string> firstNameToMach = (names, sum, match)
+    => names.FirstOrDefault(n => checkLength(n, sum));
+
+
+
+int sum = int.Parse(Console.ReadLine());
+
+List<string> names = Console.ReadLine()
+    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+    .ToList();
+
+
+
+Console.WriteLine(firstNameToMach(names, sum, checkLength));
