fefe8976312a5649ff2081dcf418f7b6345ca476
IvanAbadjiev <ivan087611@gmail.com>
2023-04-23 17:21:05 +0300

    Defining Classes
    • Defining Simple Classes;
    • Enumerations;
    • Static Classes;
    • Namespaces.

---
+++ Defining Classes/Lab/P01. Car/Car.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarManufacturer
+{
+    class Car
+
+
+    //Define in the above class private fields for:
+    //• make: string
+    //• model: string
+    //• year: int
+
+    {
+        // TODO: define the Car class members here …
+        private string make;
+        private string model;
+        private int year;
+
+
+
+        public string Make { get; set; }
+        public string Model { get; set; }
+        public int Year { get; set; }
+    }
+}
+
---
+++ Defining Classes/Lab/P01. Car/StartUp.cs
@@ -0,0 +1,18 @@
+﻿namespace CarManufacturer
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            // TODO: define the Main() method here ...
+
+            Car car = new Car();
+
+            car.Make = "VW";
+            car.Model = "MK3";
+            car.Year = 1992;
+
+            Console.WriteLine($"Make: {car.Make}{Environment.NewLine}Model: {car.Model}{Environment.NewLine}Year: {car.Year}");
+        }
+    }
+}
