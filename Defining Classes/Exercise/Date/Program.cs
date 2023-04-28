73400677a35ab9d2862ab361bb2f0e02873a90e5
IvanAbadjiev <ivan087611@gmail.com>
2023-04-28 20:11:15 +0300

    Defining Classes
    • Defining Simple Classes;
    • Enumerations;
    • Static Classes;
    • Namespaces.

---
+++ Defining Classes/Exercise/Date/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace DateModifier
+{
+
+    public static class StarUp
+    {
+        public static void Main(string[] args)
+        {
+            string start = Console.ReadLine();
+            string end = Console.ReadLine();
+
+            int different = DateModifier.GetDifference(start, end);
+
+            Console.WriteLine(different);
+
+        }
+
+    }
+}
