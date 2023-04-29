e513b92697fb16152159ae45751024895846bacc
IvanAbadjiev <ivan087611@gmail.com>
2023-04-29 21:59:03 +0300

    Defining Classes
    • Defining Simple Classes;
    • Enumerations;
    • Static Classes;
    • Namespaces.

---
+++ Defining Classes/Exercise/P01. Define a Class Person/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            // TODO: define the Main() method here ...
+            Person peter = new("Petur", 20);
+
+
+            Person george = new("Georgi", 18);
+
+
+
+            Console.WriteLine($"{peter.Name} is {peter.Age} years old");
+            Console.WriteLine($"{george.Name} is {george.Age} years old");
+
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
