e1022efc35c0f57faed0fc8d3575507e614e9bff
IvanAbadjiev <ivan087611@gmail.com>
2023-04-30 18:29:37 +0300

    Defining Classes
    • Defining Simple Classes;
    • Enumerations;
    • Static Classes;
    • Namespaces.

---
+++ Defining Classes/Exercise/P02. Creating Constructors/StartUp.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DefiningClasses
+{
+    public class StartUp
+    {
+        static void Main()
+        {
+            // TODO: define the Main() method here ...
+
+            Person personWitoutCtor = new Person();
+            Person personWitAgeCtro = new Person(20);
+            Person personWitNameAgeCtor = new Person("Petur", 20);
+
+
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
