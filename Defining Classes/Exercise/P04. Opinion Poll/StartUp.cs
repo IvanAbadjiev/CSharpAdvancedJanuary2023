c4eae4b5a5f92f55192e4bca06427e85a4074752
IvanAbadjiev <ivan087611@gmail.com>
2023-05-01 15:13:05 +0300

    Defining Classes
    • Defining Simple Classes;
    • Enumerations;
    • Static Classes;
    • Namespaces.

---
+++ Defining Classes/Exercise/P04. Opinion Poll/StartUp.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace DefiningClasses;
+
+public class StartUp
+{
+    static void Main(string[] args)
+    {
+        List<Person> person = new List<Person>();
+
+        int n = int.Parse(Console.ReadLine());
+        for (int i = 0; i < n; i++)
+        {
+            string[] personInfo = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+            string name = personInfo[0];
+            int age = int.Parse(personInfo[1]);
+            Person curentPerson = new Person(name, age);
+            person.Add(curentPerson);
+
+        }
+
+        foreach (var people in person.OrderBy(p => p.Name))
+        {
+            if (people.Age > 30)
+            {
+                Console.WriteLine($"{people.Name} - {people.Age}");
+            }
+        }
+
+
+        //Family family = new();
+
+        //int count = int.Parse(Console.ReadLine());
+
+        //for (int i = 0; i < count; i++)
+        //{
+        //    string[] personProperties = Console.ReadLine()
+        //        .Split(" ", StringSplitOptions.RemoveEmptyEntries);
+
+        //    Person person = new(personProperties[0], int.Parse(personProperties[1]));
+
+        //    family.AddMember(person);
+        //}
+
+        //Person oldest = family.GetOldestMember();
+
+        //Console.WriteLine($"{oldest.Name} {oldest.Age}");
+    }
+}
