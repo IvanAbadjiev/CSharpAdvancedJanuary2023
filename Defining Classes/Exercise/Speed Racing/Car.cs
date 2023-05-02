5843860ca02774ef5f5c9f1c0fc994856fa1d18f
IvanAbadjiev <ivan087611@gmail.com>
2023-05-03 00:09:38 +0300

    Defining Classes
    • Defining Simple Classes;
    • Enumerations;
    • Static Classes;
    • Namespaces.

---
+++ Defining Classes/Exercise/Speed Racing/Car.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RawData
+{
+    public class Car
+    {
+        private string model;
+        private double fuelAmount;
+        private double fuelConsumptionPerKilometer;
+        private double travelledDistance;
+
+
+        //public Car()
+        //{
+
+        //}
+        public Car(string model, double fuelAmount, double fuelConsumptionPerKilometer)
+        {
+            this.model = model;
+            this.fuelAmount = fuelAmount;
+            this.fuelConsumptionPerKilometer = fuelConsumptionPerKilometer;
+            TravelledDistance = 0;
+        }
+
+        public double TravelledDistance
+        {
+            get { return travelledDistance; }
+            set { travelledDistance = value; }
+        }
+
+        public double FuelConsumptionPerKilometer
+        {
+            get { return fuelConsumptionPerKilometer; }
+            set { fuelConsumptionPerKilometer = value; }
+        }
+
+        public double FuelAmount
+        {
+            get { return fuelAmount; }
+            set { fuelAmount = value; }
+        }
+
+
+        public string Model
+        {
+            get { return model; }
+            set { model = value; }
+        }
+
+
+        public double Drive(Car car, double amountOfKm)
+        {
+            double consumation = 0;
+            if (car.FuelConsumptionPerKilometer * amountOfKm > car.fuelAmount)
+            {
+                Console.WriteLine("Insufficient fuel for the drive");
+            }
+            else
+            {
+                consumation = car.FuelConsumptionPerKilometer * amountOfKm;
+            }
+            return consumation;
+
+
+        }
+    }
+}
