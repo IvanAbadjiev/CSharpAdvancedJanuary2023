3ba463f8f72c1cd7b5859d19078391bab3925219
IvanAbadjiev <ivan087611@gmail.com>
2023-05-04 23:02:13 +0300

    Generics
    • Generics;
    • Generic Classes;
    • Generic Methods;
    • Generic Constraints.

---
+++ Generics/Lab/BoxOfT/Box.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BoxOfT
+{
+    public class Box<T>
+    {
+        private List<T> element;
+        private int count;
+
+        public int Count
+        {
+            get { return count; }
+
+        }
+
+        public Box()
+        {
+            element = new List<T>();
+            count = 0;
+        }
+
+        //public List<T> Element
+        //{
+        //    get { return element; }
+        //    set { element = value; }
+        //}
+
+
+
+        public void Add(T item)
+        {
+            element.Add(item);
+            count++;
+        }
+
+        public T Remove()
+        {
+            T el = element[element.Count - 1];
+            element.RemoveAt(element.Count - 1);
+            count--;
+            return el;
+        }
+
+
+
+    }
+}
---
+++ Generics/Lab/BoxOfT/StartUp.cs
@@ -0,0 +1,21 @@
+﻿namespace BoxOfT;
+using BoxOfT;
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+
+        Box<int> box = new Box<int>();
+        box.Add(1);
+        box.Add(3);
+        box.Add(4);
+
+        Console.WriteLine(box.Remove());
+
+        box.Add(5);
+        box.Add(6);
+
+        Console.WriteLine(box.Count);
+    }
+
+}
