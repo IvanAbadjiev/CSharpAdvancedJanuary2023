ceaa9d9078016e48ff7606ddf3dd98c60854ae91
IvanAbadjiev <ivan087611@gmail.com>
2023-05-05 22:56:24 +0300

    Generics
    • Generics;
    • Generic Classes;
    • Generic Methods;
    • Generic Constraints.

---
+++ Generics/Lab/GenericScale/EqualityScale.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericScale
+{
+    public class EqualityScale<T>
+    {
+        private T left;
+        private T rigth;
+
+        public EqualityScale(T left, T rigth)
+        {
+            this.left = left;
+            this.rigth = rigth;
+        }
+
+
+        public bool AreEqual()
+        {
+
+            return left.Equals(rigth);
+
+        }
+
+    }
+}
---
+++ Generics/Lab/GenericScale/StartUp.cs
@@ -0,0 +1,12 @@
+﻿namespace GenericScale;
+
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        EqualityScale<int> ex = new EqualityScale<int>(5, 6);
+
+        Console.WriteLine(ex.AreEqual());
+    }
+
+}
