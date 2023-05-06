9e0a7aac3b959bb268417468b046bd5e707fe3fc
IvanAbadjiev <ivan087611@gmail.com>
2023-05-06 21:10:11 +0300

    Generics
    • Generics;
    • Generic Classes;
    • Generic Methods;
    • Generic Constraints.

---
+++ Generics/Exercise/GenericBoxOfString/StartUp.cs
@@ -0,0 +1,20 @@
+﻿namespace GenericBoxOfString;
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Box<String> box = new();
+
+        for (int i = 0; i < n; i++)
+        {
+            string item = Console.ReadLine();
+
+            box.Add(item);
+
+        }
+        Console.WriteLine(box.ToString());
+
+    }
+
+}
