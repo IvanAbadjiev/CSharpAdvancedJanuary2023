3141320dc3b9e0d01ff90b81055c646a1e71ef6e
IvanAbadjiev <ivan087611@gmail.com>
2023-05-06 21:10:40 +0300

    Generics
    • Generics;
    • Generic Classes;
    • Generic Methods;
    • Generic Constraints.

---
+++ Generics/Exercise/GenericCountMethodDoubles/Box.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericCountMethodDoubles
+{
+    public class Box<T> where T : IComparable<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+
+        }
+
+        public int Count(T el)
+        {
+            int count = 0;
+            foreach (T item in items)
+            {
+                if (item.CompareTo(el) > 0)
+                {
+                    count++;
+                }
+            }
+            return count;
+
+        }
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            T firstEl = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = firstEl;
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+
+            foreach (T item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
---
+++ Generics/Exercise/GenericCountMethodDoubles/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace GenericCountMethodDoubles;
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Box<double> box = new();
+
+        for (int i = 0; i < n; i++)
+        {
+            double item = double.Parse(Console.ReadLine());
+
+            box.Add(item);
+
+        }
+
+        //int[] indexes = Console.ReadLine()
+        //    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        //    .Select(int.Parse)
+        //    .ToArray();
+
+        //int firstIndex = indexes[0];
+        //int secondIndex = indexes[1];
+        //box.Swap(firstIndex, secondIndex);
+        double el = double.Parse(Console.ReadLine());
+
+        Console.WriteLine(box.Count(el));
+
+    }
+
+}
