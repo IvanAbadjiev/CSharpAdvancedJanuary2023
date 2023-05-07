8bfc65c270392e2ab0798575ed66a4a5b2c2e33e
IvanAbadjiev <ivan087611@gmail.com>
2023-05-07 19:03:21 +0300

    Generics
    • Generics;
    • Generic Classes;
    • Generic Methods;
    • Generic Constraints.

---
+++ Generics/Exercise/GenericSwapMethodIntegers/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace GenericSwapMethodIntegers;
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Box<int> box = new();
+
+        for (int i = 0; i < n; i++)
+        {
+            int item = int.Parse(Console.ReadLine());
+
+            box.Add(item);
+
+        }
+
+        int[] indexes = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        int firstIndex = indexes[0];
+        int secondIndex = indexes[1];
+        box.Swap(firstIndex, secondIndex);
+        Console.WriteLine(box.ToString());
+
+    }
+
+}
