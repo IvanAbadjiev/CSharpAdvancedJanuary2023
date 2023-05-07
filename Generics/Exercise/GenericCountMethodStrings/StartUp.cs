85ffbce0371e484a7f8c8babceaae9511b362e64
IvanAbadjiev <ivan087611@gmail.com>
2023-05-07 19:02:54 +0300

    Generics
    • Generics;
    • Generic Classes;
    • Generic Methods;
    • Generic Constraints.

---
+++ Generics/Exercise/GenericCountMethodStrings/StartUp.cs
@@ -0,0 +1,31 @@
+﻿namespace GenericCountMethodStrings;
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Box<String> box = new();
+
+        for (int i = 0; i < n; i++)
+        {
+            string item = Console.ReadLine();
+
+            box.Add(item);
+
+        }
+
+        //int[] indexes = Console.ReadLine()
+        //    .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+        //    .Select(int.Parse)
+        //    .ToArray();
+
+        //int firstIndex = indexes[0];
+        //int secondIndex = indexes[1];
+        //box.Swap(firstIndex, secondIndex);
+        string el = Console.ReadLine();
+
+        Console.WriteLine(box.Count(el));
+
+    }
+
+}
