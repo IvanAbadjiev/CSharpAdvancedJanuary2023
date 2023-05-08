8d0e9ba6d91abf7b7218a6a419ebb3295a8ab6ae
IvanAbadjiev <ivan087611@gmail.com>
2023-05-08 21:49:13 +0300

    Generics
    • Generics;
    • Generic Classes;
    • Generic Methods;
    • Generic Constraints.

---
+++ Generics/Exercise/GenericSwapMethodStrings/Box.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericSwapMethodStrings
+{
+    public class Box<T>
+    {
+        private List<T> items;
+
+        public Box()
+        {
+            items = new List<T>();
+        }
+
+        public void Add(T item)
+        {
+            items.Add(item);
+
+        }
+
+
+        public void Swap(int firstIndex, int secondIndex)
+        {
+            T firstEl = items[firstIndex];
+            items[firstIndex] = items[secondIndex];
+            items[secondIndex] = firstEl;
+
+        }
+
+        public override string ToString()
+        {
+            StringBuilder sb = new();
+
+            foreach (T item in items)
+            {
+                sb.AppendLine($"{typeof(T)}: {item}");
+            }
+            return sb.ToString().TrimEnd();
+        }
+    }
+}
+
---
+++ Generics/Exercise/GenericSwapMethodStrings/StartUp.cs
@@ -0,0 +1,29 @@
+﻿namespace GenericSwapMethodStrings;
+public class StartUp
+{
+    public static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        Box<String> box = new();
+
+        for (int i = 0; i < n; i++)
+        {
+            string item = Console.ReadLine();
+
+            box.Add(item);
+
+        }
+
+        int[] indexes = Console.ReadLine()
+            .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        int firstIndex = indexes[0];
+        int secondIndex = indexes[1];
+        box.Swap(firstIndex, secondIndex);
+        Console.WriteLine(box.ToString());
+
+    }
+
+}
